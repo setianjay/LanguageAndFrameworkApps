bb7b8edbf298dd3f1391264805d63ddd7d143cc2
setianjay <hari.setiaji2000@gmail.com>
2021-05-23 18:02:54 +0700

    Initial commit. Create function in SplashScreen.kt to move layout SplashScreen to layout MainActivity

--- app/src/main/java/com/setianjay/languageandframeworkapps/SplashScreen.kt
+++ app/src/main/java/com/setianjay/languageandframeworkapps/SplashScreen.kt
@@ -1,11 +1,23 @@
 package com.setianjay.languageandframeworkapps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        moveToMain()
+    }
+
+    private fun moveToMain(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(applicationContext,MainActivity::class.java))
+            finish()
+        }, 2000L)
     }
 }
