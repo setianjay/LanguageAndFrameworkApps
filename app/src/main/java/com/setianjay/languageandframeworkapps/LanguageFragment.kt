d6d37ea5010a745d5bcec3509c718f2db6cbbe40
setianjay <hari.setiaji2000@gmail.com>
2021-05-26 14:20:10 +0700

    Initial commit. Implement viewBinding in LanguageFragment

--- app/src/main/java/com/setianjay/languageandframeworkapps/LanguageFragment.kt
+++ app/src/main/java/com/setianjay/languageandframeworkapps/LanguageFragment.kt
@@ -1,19 +1,24 @@
 package com.setianjay.languageandframeworkapps
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import com.setianjay.languageandframeworkapps.databinding.FragmentLanguageBinding
 
 
 class LanguageFragment : Fragment() {
+    private lateinit var binding: FragmentLanguageBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_language, container, false)
+        binding = FragmentLanguageBinding.inflate(inflater,container,false)
+        return binding.root
     }
 }
