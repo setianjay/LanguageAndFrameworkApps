91d207c3cd2663441d91efd59cf0edae76787f78
setianjay <hari.setiaji2000@gmail.com>
2021-05-30 18:02:33 +0700

    Initial commit. Implement viewBinding in FrameworkFragment

--- app/src/main/java/com/setianjay/languageandframeworkapps/FrameworkFragment.kt
+++ app/src/main/java/com/setianjay/languageandframeworkapps/FrameworkFragment.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.setianjay.languageandframeworkapps.databinding.FragmentFrameworkBinding
 
 class FrameworkFragment : Fragment() {
+    private lateinit var binding: FragmentFrameworkBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_framework, container, false)
+        binding = FragmentFrameworkBinding.inflate(inflater,container,false)
+        return binding.root
     }
 }
