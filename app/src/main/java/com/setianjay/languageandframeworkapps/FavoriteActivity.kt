fea393e8b3f13cd29bfea5937a7a2445e1a3678d
setianjay <hari.setiaji2000@gmail.com>
2021-06-13 20:25:01 +0700

    Initial commit. Create FavoriteActivity

---
+++ app/src/main/java/com/setianjay/languageandframeworkapps/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.setianjay.languageandframeworkapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorit)
+    }
+}
